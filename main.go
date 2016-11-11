57223454a80f2fc5041321e03aed0ba3a1841257
Dmitry Yakutkin <dmitry.yakutkin@gmail.com>
2016-11-11 12:40:10 +0200

    libcalico-go internal logging is disabled

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 
 	"github.com/docker/go-plugins-helpers/ipam"
 	"github.com/docker/go-plugins-helpers/network"
+	"github.com/pkg/errors"
 	"github.com/projectcalico/libcalico-go/lib/api"
 	"github.com/projectcalico/libnetwork-plugin/driver"
 
-	datastoreClient "github.com/projectcalico/libcalico-go/lib/client"
 	"flag"
 	"fmt"
+
+	datastoreClient "github.com/projectcalico/libcalico-go/lib/client"
 )
 
 const (
@@ -21,29 +23,33 @@ const (
 )
 
 var (
-	config    *api.ClientConfig
-	client    *datastoreClient.Client
+	config *api.ClientConfig
+	client *datastoreClient.Client
 
 	logger *log.Logger
 )
 
 func init() {
-	var err error
+	logger = log.New(os.Stdout, "", log.LstdFlags)
+}
 
+func initializeClient() {
+	var err error
 	if config, err = datastoreClient.LoadClientConfig(""); err != nil {
+		err = errors.Wrap(err, "Config loading error")
 		panic(err)
 	}
 	if client, err = datastoreClient.New(*config); err != nil {
+		err = errors.Wrap(err, "Client instantiation error")
 		panic(err)
 	}
-
-	logger = log.New(os.Stdout, "", log.LstdFlags)
 }
 
 // VERSION is filled out during the build process (using git describe output)
 var VERSION string
 
 func main() {
+
 	// Display the version on "-v"
 	// Use a new flag set so as not to conflict with existing libraries which use "flag"
 	flagSet := flag.NewFlagSet("Calico", flag.ExitOnError)
@@ -58,6 +64,8 @@ func main() {
 		fmt.Println(VERSION)
 		os.Exit(0)
 	}
+
+	initializeClient()
 
 	errChannel := make(chan error)
 	networkHandler := network.NewHandler(driver.NewNetworkDriver(client, logger))
